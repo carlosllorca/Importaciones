a76914c8c6576122d22bb18ad6103e4dd9bc9492
carlos <carlosllorca89@gmail.com>
2020-02-23 03:07:35 -0500

    Antes de iniciar el proceso de licitación

--- views/um/view.php
+++ views/um/view.php
@@ -11,27 +11,33 @@ $this->params['breadcrumbs'][] = ['label' => 'Ums', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="um-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
+
+<div class="card">
+    <div class="card-header card-header-primary">
+        <h4 class="card-title"><?=$this->title?></h4>
+
+    </div>
+    <div class="card-body" style="padding: 15px">
+        <p>
+            <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
+
+        <?= DetailView::widget([
+            'model' => $model,
+            'attributes' => [
+                'id',
+                'label',
             ],
         ]) ?>
-    </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'label',
-        ],
-    ]) ?>
-
+    </div>
 </div>
